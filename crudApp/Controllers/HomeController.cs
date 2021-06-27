7026eb0e1214547c544b5ea89137bf9ef9ebee3b
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-06-27 14:12:30 +0300

    feat: completed Add method

--- crudApp/Controllers/HomeController.cs
+++ crudApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace crudApp.Controllers
 			return View();
 		}
 
+		private int Add(int num1, int num2)
+		{
+			return num1 + num2;
+		}
+
 		public IActionResult Privacy()
 		{
 			return View();
